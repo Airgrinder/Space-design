aed0a6aba85d4f9e1d2ad357265eaa546d683c9a
Airgrinder <51jework@mail.com>
2020-11-10 02:07:14 +0300

    Updates

---
+++ precache-manifest.a7262d94b396124ecf1322b9a1558411.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214c81eed81fb7d3a1874d2321136df6",
+    "url": "/Space-design/index.html"
+  },
+  {
+    "revision": "abc3595d2d2391bc3957",
+    "url": "/Space-design/static/css/main.ebe755b4.chunk.css"
+  },
+  {
+    "revision": "0eda633b45ad0161edca",
+    "url": "/Space-design/static/js/2.9fe091aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Space-design/static/js/2.9fe091aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc3595d2d2391bc3957",
+    "url": "/Space-design/static/js/main.e1751c6a.chunk.js"
+  },
+  {
+    "revision": "805eb7aeb5ed996e81e1",
+    "url": "/Space-design/static/js/runtime-main.4b03bb29.js"
+  },
+  {
+    "revision": "81af0f2f7ec5d6df0924916c965f7f20",
+    "url": "/Space-design/static/media/Example1.81af0f2f.svg"
+  },
+  {
+    "revision": "c57bbe4360d9896a59f5467feaf266dd",
+    "url": "/Space-design/static/media/Example2.c57bbe43.svg"
+  },
+  {
+    "revision": "2a6974cc20ff25b6e76d43cd57223230",
+    "url": "/Space-design/static/media/Example3.2a6974cc.svg"
+  },
+  {
+    "revision": "3636e259217e0bae63129d2884423002",
+    "url": "/Space-design/static/media/Example4.3636e259.svg"
+  },
+  {
+    "revision": "db1c419dca4884bd5b31360296a9254c",
+    "url": "/Space-design/static/media/Examplemain.db1c419d.svg"
+  },
+  {
+    "revision": "c6c7c13fdf796b92576d6d5642a84e99",
+    "url": "/Space-design/static/media/arrow.c6c7c13f.svg"
+  },
+  {
+    "revision": "ea17e82a7b3e29b39c3cad0f05bb3c9c",
+    "url": "/Space-design/static/media/decor.ea17e82a.svg"
+  },
+  {
+    "revision": "48ec53d9ec5fff92c8f7ae137508cd25",
+    "url": "/Space-design/static/media/figure13.48ec53d9.svg"
+  },
+  {
+    "revision": "ca0b94d25418284456b37ce276c38029",
+    "url": "/Space-design/static/media/footer.ca0b94d2.svg"
+  },
+  {
+    "revision": "3402780cca10cc553d3786bd392a32e7",
+    "url": "/Space-design/static/media/header.3402780c.svg"
+  },
+  {
+    "revision": "7a22d173da5fa7e7cd95c005baa64068",
+    "url": "/Space-design/static/media/mailicon.7a22d173.svg"
+  },
+  {
+    "revision": "6fcec6a9bfd644658984d3cb2ab58a1d",
+    "url": "/Space-design/static/media/markicon.6fcec6a9.svg"
+  },
+  {
+    "revision": "26863fcd135dde6dd332fb545ccf358b",
+    "url": "/Space-design/static/media/phone1.26863fcd.svg"
+  },
+  {
+    "revision": "6ca5d2aa2d534cbae98bd24c982a4d6e",
+    "url": "/Space-design/static/media/photobuild.6ca5d2aa.svg"
+  },
+  {
+    "revision": "4b3ace56eaebce29b763ad612c114365",
+    "url": "/Space-design/static/media/photos.4b3ace56.svg"
+  },
+  {
+    "revision": "9a24e3961f3ae56634e94d70ec2bca8f",
+    "url": "/Space-design/static/media/portfolioimg1.9a24e396.svg"
+  },
+  {
+    "revision": "ad2deef2253694f0651da0f94d5fcedc",
+    "url": "/Space-design/static/media/portfolioimg2.ad2deef2.svg"
+  },
+  {
+    "revision": "739b80749313ba7be894fce133c4d790",
+    "url": "/Space-design/static/media/portfolioimg3.739b8074.svg"
+  },
+  {
+    "revision": "2d3dedf57b68b5b14d46c5deec19829a",
+    "url": "/Space-design/static/media/portfolioimg4.2d3dedf5.svg"
+  },
+  {
+    "revision": "96753edd76f9ea9ae7f04c6e0b108718",
+    "url": "/Space-design/static/media/project1.96753edd.svg"
+  },
+  {
+    "revision": "9df9714b77ca18ac209a5b8af894e4b5",
+    "url": "/Space-design/static/media/project2.9df9714b.svg"
+  },
+  {
+    "revision": "ac28531d5b37ae144b50e87916570869",
+    "url": "/Space-design/static/media/project3.ac28531d.svg"
+  },
+  {
+    "revision": "09a0c74f8f2c8f954a903ebff5b42851",
+    "url": "/Space-design/static/media/project4.09a0c74f.svg"
+  },
+  {
+    "revision": "65a1d6d324245f5558c612f40fb890ac",
+    "url": "/Space-design/static/media/project5.65a1d6d3.svg"
+  },
+  {
+    "revision": "f8c56be0432be8b0826b2e970d2f1782",
+    "url": "/Space-design/static/media/project6.f8c56be0.svg"
+  },
+  {
+    "revision": "670fe9807de8b74362513093d4c1924b",
+    "url": "/Space-design/static/media/project7.670fe980.svg"
+  },
+  {
+    "revision": "69f5c6ba30c9db3806a5fa959b682a6a",
+    "url": "/Space-design/static/media/telephoneicon.69f5c6ba.svg"
+  }
+]);
